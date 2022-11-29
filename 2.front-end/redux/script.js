395a897b5bdefdf9449fe5de0cc2cfa6be89e243
cauan-oss <cauanbarros14@gmail.com>
2022-11-29 18:20:05 +0000

    criando action

--- 2.front-end/redux/script.js
+++ 2.front-end/redux/script.js
@@ -1,4 +1,12 @@
 import { legacy_createStore as createStore } from "redux"; 
+import { composeWithDevTools } from "@redux-devtools/extension/lib/types/logOnly";
+//criando o action 
+const stateInitial = {count: 0};
+
+const reducer = (state = stateInitial, action) => {
+  return state; 
+}
+const store = createStore(reducer)
 
 const incrementButton = document.querySelector('button'); 
 incrementButton.addEventListener('click', () => {
