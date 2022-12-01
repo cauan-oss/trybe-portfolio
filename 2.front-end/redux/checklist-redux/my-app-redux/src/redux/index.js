780a1d64ea5ef23db07a24db742ecf9e35b9f12a
cauan-oss <cauanbarros14@gmail.com>
2022-12-01 17:40:12 +0000

    store criado

--- 2.front-end/redux/checklist-redux/my-app-redux/src/redux/index.js
+++ 2.front-end/redux/checklist-redux/my-app-redux/src/redux/index.js
@@ -1,1 +1,6 @@
-//iniciando o redux
+import { legacy_createStore as createStore } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
+import rootReducers from './reducers'
+const store = createStore(rootReducers, composeWithDevTools())
+
+export default store;
