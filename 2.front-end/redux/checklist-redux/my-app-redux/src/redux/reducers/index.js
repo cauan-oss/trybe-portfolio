920f38c2b33fd207ba85ffa974a83fcd6d96c453
cauan-oss <cauanbarros14@gmail.com>
2022-12-01 17:46:46 +0000

    meu carro e hyunday

--- 2.front-end/redux/checklist-redux/my-app-redux/src/redux/reducers/index.js
+++ 2.front-end/redux/checklist-redux/my-app-redux/src/redux/reducers/index.js
@@ -1,1 +1,16 @@
-//iniciando o reducer 
+import { combineReducers } from "redux";
+
+const INITIAL_STATE = {};
+
+const exampleReducers = (state = INITIAL_STATE, action) => {
+    switch (action.type) {
+        default: 
+         return state;
+    }
+}
+
+const rootReducers = combineReducers({ exampleReducers })
+
+export default rootReducers;
+
+
