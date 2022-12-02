57b9b22a754571fc638f4b0cdabe7a8872746f7e
cauan-oss <cauanbarros14@gmail.com>
2022-12-02 19:23:32 +0000

    buscador e botao criados

--- 2.front-end/project-personal-CEP/buscador/src/App.jsx
+++ 2.front-end/project-personal-CEP/buscador/src/App.jsx
@@ -1,8 +1,15 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1>teste projeto</h1>
+    <div className="container">
+      <h1 className="title">teste projeto</h1>
+      <div className="container-input">
+        <label htmlFor="input-text">
+         <input placeholder="Digite seu cep..." id="input-text" type="text" />
+         <button className="button-search">Procurar</button>
+
+        </label>
+      </div>
     </div>
   );
 }
