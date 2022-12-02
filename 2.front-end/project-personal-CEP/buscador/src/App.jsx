08b698a8b73d1652aa1260496cb5799ff021f443
cauan-oss <cauanbarros14@gmail.com>
2022-12-02 19:34:05 +0000

    ceps criado

--- 2.front-end/project-personal-CEP/buscador/src/App.jsx
+++ 2.front-end/project-personal-CEP/buscador/src/App.jsx
@@ -1,3 +1,4 @@
+import { FiSearch } from 'react-icons/fi';
 
 function App() {
   return (
@@ -6,10 +7,20 @@ function App() {
       <div className="container-input">
         <label htmlFor="input-text">
          <input placeholder="Digite seu cep..." id="input-text" type="text" />
-         <button className="button-search">Procurar</button>
-
+         <button className="button-search"> 
+         <FiSearch size={25} color='white'
+         ></FiSearch>
+         </button>
         </label>
       </div>
+      <main className='main'>
+        <h2> cep</h2>
+        <span>logradouro</span>
+        <span>complemento</span>
+        <span>bairro</span>
+        <span>localidade-estado</span>
+
+      </main>
     </div>
   );
 }
