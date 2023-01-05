c4c2ca98ca81ef3a9660f35a870a5113df67dd73
cauan-oss <cauanbarros14@gmail.com>
2023-01-05 09:52:32 +0000

    increment-button

---
+++ 2.front-end/increment-button/my-app/src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>contador</h1>
+     <p>0</p>
+     <button>Incrementar</button>
+    </div>
+  );
+}
+
+export default App;
