a5dea8c0bc4574baa432f6dbc270ae8fc64ba27b
cauan-oss <cauanbarros14@gmail.com>
2023-01-05 21:37:52 +0000

    sercidor rodando

--- 3.back-end/system-login/index.js
+++ 3.back-end/system-login/index.js
@@ -1,2 +1,16 @@
 const express = require('express');
 const session = require('express-session');
+
+const port = 3000;
+const path = require('path');
+const app = express();
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, '/views'));
+
+app.listen(port, () => {
+    console.log('servidor rodando')
+});
+
