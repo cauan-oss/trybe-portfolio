f860cfc4c9464714b4159f3030c52422d7f9b555
cauan-oss <cauanbarros14@gmail.com>
2023-01-18 14:59:48 +0000

    finalizado

--- 2.front-end/trybeMail/my-app/src/App.jsx
+++ 2.front-end/trybeMail/my-app/src/App.jsx
@@ -1,14 +1,12 @@
 import './App.css';
-import { useState } from 'react';
+
 import Email from './components/Email';
 
 function App() {
-  const [email, setEmail] = useState({ id: 1, title: 'titulo do email', status: 0 })
   
-
   return (
     <div className="App">
-      <Email state={ { email, setEmail } } />
+      <Email  />
      </div>
   );
 }
--- 2.front-end/trybeMail/my-app/src/components/Email.jsx
+++ 2.front-end/trybeMail/my-app/src/components/Email.jsx
@@ -1,22 +1,97 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useState } from "react";
 
 function Email({ state }) {
-    
+  const [emails, setEmail] = useState([
+    { id: 1, title: "Ola tenho uma vaga que bate com o seu perfil", status: 0 },
+    {
+      id: 2,
+      title:
+        " Parabens voce avancou no processo da vaga de pessoa desenvolvedora",
+      status: 0,
+    },
+  ]);
+  const click = (id) => {
+    setEmail(
+      emails.map((email) => {
+        if (email.id === id) {
+          email.status = 1;
+        }
+        return email;
+      })
+    );
+  };
+
+  const unreadEmail = (id) => {
+    setEmail(
+      emails.map((email) => {
+        if (email.id === id) {
+          email.status = 0;
+        }
+        return email;
+      })
+    );
+  };
+  const everythingEmails = () => {
+    setEmail(
+      emails.map((email) => {
+        email.status = 1;
+        return email;
+      })
+    );
+  };
+  const everythingUnreadEmails = () => {
+    setEmail(
+      emails.map((email) => {
+        email.status = 0;
+        return email;
+      })
+    );
+  };
+
+  /* useEffect(() => {}, []) Caso prescise um estado alterar outro estado o stato fica dentro do 
+    array para ser monitorado */
+  useEffect(() => {
+    if (emails.every((email) => email.status === 1)) {
+      alert("parabens voce leu todos os emails");
+    }
+  }, [emails]);
   return (
     <div>
-       <h1 id='title'>TrybeMail</h1>
-     <div id='botoes'>
-      <br />
-     <button type='button'>Marcar todas como lida</button>
-     <button type='button'>Marcar todas como nao lida</button>
-     </div>
-     <div  id='emails'>
-
-      <p> Ola tenho uma vaga que bate com o seu perfil</p>
-      <p>Parabens voce avancou no processo da vaga de pessoa desenvolvedora</p>
+      <header>
+        <h1 id="title">TrybeMail</h1>
+      </header>
+      <div id="botoes">
+        <button onClick={everythingEmails} type="button">
+          {" "}
+          todos lidos{" "}
+        </button>
+        <button onClick={everythingUnreadEmails} type="button">
+          {" "}
+          todos nao lidos{" "}
+        </button>
+        <br />
+      </div>
+      <div id="emails">
+        {emails.map((email) => (
+          <label key={email.id} htmlFor="">
+            <p style={{ background: email.status ? "gray" : "inherit" }}>
+              {email.title}
+            </p>
+            <button onClick={() => click(email.id)} type="button">
+              {" "}
+              marcar como lido
+            </button>
+            <button onClick={() => unreadEmail(email.id)}>
+              marcar como nao lido
+            </button>
+          </label>
+        ))}
+      </div>
     </div>
-    </div>
-  )
-};
+  );
+}
 
+/* quando eu passo paramentros dentro da funcao onclick tem que ser em callback
+marcar como lido*/
 export default Email;
